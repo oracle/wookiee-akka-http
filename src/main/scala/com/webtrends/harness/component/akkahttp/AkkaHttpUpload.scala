236991188b80e37f7a6124ad7eee2047c0605fc4
Jonathan Hasbun <hasbunj@pdxhasbunj01.webtrends.corp>
2016-08-30 13:47:55 -0700

    Updated fileupload

--- src/main/scala/com/webtrends/harness/component/akkahttp/AkkaHttpUpload.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/AkkaHttpUpload.scala
@@ -1,5 +1,9 @@
 package com.webtrends.harness.component.akkahttp
 
+import akka.http.scaladsl.server.directives.PathDirectives
+import akka.http.scaladsl.server.directives.FileUploadDirectives.uploadedFile
+import com.webtrends.harness.command.Command
+
 
 trait AkkaHttpUpload extends AkkaHttpPost {
   this: Command =>
