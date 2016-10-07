929a15cc9c4612e97d9a1909e6b1075200c1285f
Ladinu Chandrasinghe <ladinu.chandrasinghe@webtrends.com>
2016-10-07 14:51:21 -0700

    explicit internal and external servers

--- src/main/scala/com/webtrends/harness/component/akkahttp/AkkaHttp.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/AkkaHttp.scala
@@ -15,50 +15,48 @@ trait AkkaHttp {
 
   val settings = AkkaHttpSettings(config)
 
-  var AkkaHttpRef: Option[ActorRef] = None
-  var InternalAkkaHttpRef: Option[ActorRef] = None
+  var internalAkkaHttpRef: Option[ActorRef] = None
+  var externalAkkaHttpRef: Option[ActorRef] = None
 
   def startAkkaHttp() = {
-    val ref = context.actorOf(AkkaHttpActor.props(settings.external), AkkaHttp.AkkaHttpName)
-    AkkaHttpRef = Some(ref)
-    if (settings.internal.enabled) {
-      val internalRef = context.actorOf(InternalAkkaHttpActor.props(settings.internal), AkkaHttp.InternalAkkaHttpName)
-      InternalAkkaHttpRef = Some(internalRef)
+    internalAkkaHttpRef = Some(context.actorOf(InternalAkkaHttpActor.props(settings.internal), AkkaHttp.InternalAkkaHttpName))
+    if (settings.external.enabled) {
+      externalAkkaHttpRef = Some(context.actorOf(ExternalAkkaHttpActor.props(settings.external), AkkaHttp.externalAkkaHttpName))
     }
   }
 
   def stopAkkaHttp() = {
-    Seq(AkkaHttpRef, InternalAkkaHttpRef).flatten.foreach(_ ! AkkaHttpUnbind)
+    Seq(internalAkkaHttpRef, externalAkkaHttpRef).flatten.foreach(_ ! AkkaHttpUnbind)
   }
 
 }
 
-final case class InternalAkkaHttpSettings(enabled: Boolean, interface: String, port: Int, serverSettings: ServerSettings)
-final case class ExternalAkkaHttpSettings(interface: String, port: Int, serverSettings: ServerSettings)
+final case class InternalAkkaHttpSettings(interface: String, port: Int, serverSettings: ServerSettings)
+final case class ExternalAkkaHttpSettings(enabled: Boolean, interface: String, port: Int, serverSettings: ServerSettings)
 final case class AkkaHttpSettings(internal: InternalAkkaHttpSettings, external: ExternalAkkaHttpSettings)
 
 object AkkaHttpSettings {
   def apply(config: Config): AkkaHttpSettings = {
-    val port = ConfigUtil.getDefaultValue("wookiee-akka-http.server.http-port", config.getInt, 8080)
-    val interface = ConfigUtil.getDefaultValue("wookiee-akka-http.server.interface", config.getString, "0.0.0.0")
+    val internalPort = ConfigUtil.getDefaultValue("wookiee-akka-http.internal-server.http-port", config.getInt, 8080)
+    val internalInterface = ConfigUtil.getDefaultValue("wookiee-akka-http.internal-server.interface", config.getString, "127.0.0.0")
 
-    val internalServerEnabled = ConfigUtil.getDefaultValue(
-      "wookiee-akka-http.internal-server.enabled", config.getBoolean, true)
-    val internalPort = ConfigUtil.getDefaultValue(
-      "wookiee-akka-http.internal-server.http-port", config.getInt, 8081)
-    val internalInterface = ConfigUtil.getDefaultValue(
-      "wookiee-akka-http.internal-server.interface", config.getString, "127.0.0.1")
+    val externalServerEnabled = ConfigUtil.getDefaultValue(
+      "wookiee-akka-http.external-server.enabled", config.getBoolean, false)
+    val externalPort = ConfigUtil.getDefaultValue(
+      "wookiee-akka-http.external-server.http-port", config.getInt, 8082)
+    val externalInterface = ConfigUtil.getDefaultValue(
+      "wookiee-akka-http.external-server.interface", config.getString, "0.0.0.0")
 
     val serverSettings = ServerSettings(config)
 
     AkkaHttpSettings(
-      InternalAkkaHttpSettings(internalServerEnabled, internalInterface, internalPort, serverSettings),
-      ExternalAkkaHttpSettings(interface, port, serverSettings)
+      InternalAkkaHttpSettings(internalInterface, internalPort, serverSettings),
+      ExternalAkkaHttpSettings(externalServerEnabled, externalInterface, externalPort, serverSettings)
     )
   }
 }
 
 object AkkaHttp {
-  val AkkaHttpName = "AkkaHttp"
+  val externalAkkaHttpName = "ExternalAkkaHttp"
   val InternalAkkaHttpName = "InternalAkkaHttp"
 }
--- src/main/scala/com/webtrends/harness/component/akkahttp/ExternalAkkaHttpActor.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/ExternalAkkaHttpActor.scala
@@ -1,9 +1,6 @@
-/*
- * Copyright (c) 2014. Webtrends (http://www.webtrends.com)
- */
 package com.webtrends.harness.component.akkahttp
 
-import akka.actor._
+import akka.actor.Props
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.RouteResult
@@ -12,25 +9,20 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
 import com.webtrends.harness.app.HActor
 import com.webtrends.harness.component.StopComponent
-import com.webtrends.harness.health.HealthComponent
 
-import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-
-object AkkaHttpActor {
+object ExternalAkkaHttpActor {
   def props(settings: ExternalAkkaHttpSettings) = {
-    Props(classOf[AkkaHttpActor], settings.port, settings.interface, settings.serverSettings)
+    Props(classOf[ExternalAkkaHttpActor], settings.port, settings.interface, settings.serverSettings)
   }
 }
 
-case class AkkaHttpUnbind()
+class ExternalAkkaHttpActor(port: Int, interface: String, settings: ServerSettings) extends HActor {
 
-class AkkaHttpActor(port: Int, interface: String, settings: ServerSettings) extends HActor {
   implicit val system = context.system
   implicit val executionContext = context.dispatcher
   implicit val materializer = ActorMaterializer()
-
 
   val serverSource = Http().bind(interface, port, settings = settings)
 
@@ -45,18 +37,12 @@ class AkkaHttpActor(port: Int, interface: String, settings: ServerSettings) exte
       log.error(s"Failed to bind akka-http external-server: $f")
   }
 
+  def unbind = bindingFuture.flatMap(_.unbind())
 
   def routes = AkkaHttpRouteContainer.getRoutes.reduceLeft(_ ~ _)
-
-  def unbind = {
-    bindingFuture.flatMap(_.unbind())
-  }
 
   override def receive = super.receive orElse {
     case AkkaHttpUnbind => unbind
     case StopComponent => unbind
   }
-
-  // This should probably be overriden to get some custom information about the health of this actor
-  override protected def getHealth: Future[HealthComponent] = super.getHealth
 }
