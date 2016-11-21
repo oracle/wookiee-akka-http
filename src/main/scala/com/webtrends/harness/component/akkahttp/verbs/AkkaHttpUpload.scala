0412599b7a8e0130376e06c3adef36d10f3779ab
Ladinu Chandrasinghe <ladinu.chandrasinghe@webtrends.com>
2016-11-21 13:48:05 -0800

    upload command wip

--- src/main/scala/com/webtrends/harness/component/akkahttp/verbs/AkkaHttpUpload.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/verbs/AkkaHttpUpload.scala
@@ -1,18 +1,30 @@
 package com.webtrends.harness.component.akkahttp.verbs
 
+import akka.http.scaladsl.server.Directives.{path => p, _}
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.directives.FileUploadDirectives.uploadedFile
-import com.webtrends.harness.command.{CommandBean, BaseCommand}
+import akka.http.scaladsl.server.directives.FileInfo
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import com.webtrends.harness.command.{BaseCommand, CommandBean}
 import com.webtrends.harness.component.akkahttp.AkkaHttpBase
 
 
 trait AkkaHttpUpload extends AkkaHttpBase {
   this: BaseCommand =>
 
-  override protected def commandInnerDirective[T <: AnyRef : Manifest](bean: CommandBean): Route =
-    uploadedFile("csv") { case (fileInfo, file) =>
-      bean.addValue("file", file)
-      bean.addValue("file-info", fileInfo)
+  def fileField: String = "file"
+  def fileSizeBytes: Long = 2.5e8.toLong
+
+  override protected def commandInnerDirective[T <: AnyRef : Manifest](bean: CommandBean): Route = {
+    (withSizeLimit(fileSizeBytes) & fileUpload(fileField)) { case (fileInfo: FileInfo, fileStream: Source[ByteString, Any]) =>
+      bean.addValue(AkkaHttpUpload.FileInfo, fileInfo)
+      bean.addValue(AkkaHttpUpload.FileStream, fileStream)
       super.commandInnerDirective(bean)
+    }
   }
+}
+
+object AkkaHttpUpload {
+  val FileInfo = "file-info"
+  val FileStream = "file-stream"
 }
