ebf0b811561b416daa8b52d3b8dea5bc7751a99f
Spencer Wood <spencer.wood@oracle.com>
2017-05-25 11:39:18 -0700

    Keep insertion order

--- src/main/scala/com/webtrends/harness/component/akkahttp/ExternalAkkaHttpRouteContainer.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/ExternalAkkaHttpRouteContainer.scala
@@ -7,7 +7,7 @@ import akka.http.scaladsl.server.Route
 import scala.collection.JavaConversions._
 
 object ExternalAkkaHttpRouteContainer {
-  private val routes = Collections.synchronizedSet[Route](new java.util.HashSet[Route]())
+  private val routes = Collections.synchronizedSet[Route](new java.util.LinkedHashSet[Route]())
   def isEmpty = routes.isEmpty
   def addRoute(r: Route) = routes.add(r)
   def getRoutes = routes.toSet
