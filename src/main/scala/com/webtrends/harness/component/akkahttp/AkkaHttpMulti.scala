45e2cdd4b595407753f16ef8afcea36a4377acc8
Spencer Wood <spencer.wood@oracle.com>
2017-06-06 09:44:48 -0700

    Fixing multiple segments case, was matching too early

--- src/main/scala/com/webtrends/harness/component/akkahttp/AkkaHttpMulti.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/AkkaHttpMulti.scala
@@ -33,19 +33,19 @@ trait AkkaHttpMulti extends AkkaHttpBase { this: BaseCommand =>
               (x match {
                 case p: String => p / Segment
                 case p: PathMatcher[Unit] if segCount == 1 => p / Segment
-                case p: PathMatcher[Tuple1[String]] => p / Segment
-                case p: PathMatcher[(String, String)] => p / Segment
-                case p: PathMatcher[(String, String, String)] => p / Segment
-                case p: PathMatcher[(String, String, String, String)] => p / Segment
+                case p: PathMatcher[Tuple1[String]] if segCount == 2 => p / Segment
+                case p: PathMatcher[(String, String)] if segCount == 3 => p / Segment
+                case p: PathMatcher[(String, String, String)] if segCount == 4 => p / Segment
+                case p: PathMatcher[(String, String, String, String)] if segCount == 5 => p / Segment
               }).asInstanceOf[PathMatcher[_]]
             case s1: String =>
               (x match {
                 case p: String => p / s1
-                case p: PathMatcher[Unit] if segCount == 0 => p / Segment
-                case p: PathMatcher[Tuple1[String]] => p / s1
-                case p: PathMatcher[(String, String)] => p / s1
-                case p: PathMatcher[(String, String, String)] => p / s1
-                case p: PathMatcher[(String, String, String, String)] => p / s1
+                case p: PathMatcher[Unit] if segCount == 0 => p / s1
+                case p: PathMatcher[Tuple1[String]] if segCount == 1 => p / s1
+                case p: PathMatcher[(String, String)] if segCount == 2 => p / s1
+                case p: PathMatcher[(String, String, String)] if segCount == 3 => p / s1
+                case p: PathMatcher[(String, String, String, String)] if segCount == 4 => p / s1
               }).asInstanceOf[PathMatcher[_]]
           }
         }
