d540e9a44c43b3f3e7eb9617db5081d0f2101aa7
malibuworkcrew <malibuworkcrew@gmail.com>
2017-07-24 13:59:22 -0700

    Support for closing sockets and sending closed message to method

--- src/test/scala/com/webtrends/harness/component/akkahttp/AkkaHttpWebsocketTest.scala
+++ src/test/scala/com/webtrends/harness/component/akkahttp/AkkaHttpWebsocketTest.scala
@@ -2,13 +2,14 @@ package com.webtrends.harness.component.akkahttp
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.Props
+import akka.actor.{ActorRef, Props}
 import akka.http.scaladsl.model.ws.{BinaryMessage, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
 import akka.stream.scaladsl.{Sink, Source}
 import akka.util.{ByteString, Timeout}
 import com.webtrends.harness.command.CommandBean
+import com.webtrends.harness.component.akkahttp.websocket.AkkaHttpWebsocket
 import org.scalatest.{MustMatchers, WordSpecLike}
 
 import scala.concurrent.Await
@@ -17,7 +18,7 @@ import scala.concurrent.duration._
 class TestWebsocket extends AkkaHttpWebsocket {
   override def path = "greeter/$var1"
 
-  override def handleText(text: String, bean: CommandBean): Option[TextMessage] = {
+  override def handleText(text: String, bean: CommandBean, callback: ActorRef): Option[TextMessage] = {
     Some(TextMessage(s"Hello $text! var1: ${bean("var1")}"))
   }
 }
@@ -25,7 +26,7 @@ class TestWebsocket extends AkkaHttpWebsocket {
 class TestWebsocketLong extends AkkaHttpWebsocket {
   override def path = "long/$var1/url/$var2/$var3/test"
 
-  override def handleText(text: String, bean: CommandBean): Option[TextMessage] = {
+  override def handleText(text: String, bean: CommandBean, callback: ActorRef): Option[TextMessage] = {
     if (text == "empty") None
     else Some(TextMessage(Source.single(s"$text! ${bean("var1")} ${bean("var2")} ${bean("var3")}")))
   }
@@ -34,15 +35,31 @@ class TestWebsocketLong extends AkkaHttpWebsocket {
 class TestWebsocketStream extends AkkaHttpWebsocket {
   override def path = "stream"
 
-  override def handleTextStream(text: Source[String, _], bean: CommandBean): Option[TextMessage] = {
+  override def handleTextStream(text: Source[String, _], bean: CommandBean, callback: ActorRef): Option[TextMessage] = {
     val ret = text.runWith(Sink.head[String])
     Some(TextMessage(Await.result(ret, checkTimeout.duration)))
   }
 
   // We have to override this since it's the standard case, but we'll never hit it if we're using streaming
-  override def handleText(text: String, bean: CommandBean): Option[TextMessage] = None
+  override def handleText(text: String, bean: CommandBean, callback: ActorRef): Option[TextMessage] = None
 
   override def isStreamingText = true
+}
+
+class TestWebsocketClose extends AkkaHttpWebsocket {
+  override def path = "close"
+
+  override def handleText(text: String, bean: CommandBean, callback: ActorRef): Option[TextMessage] = {
+    Some(TextMessage("Close Actor"))
+  }
+
+  override def closeWebsocket(bean: CommandBean, callback: Option[ActorRef]) = {
+    ClosedObject.closed = true
+  }
+}
+
+object ClosedObject {
+  @volatile var closed = false
 }
 
 class AkkaHttpWebsocketTest extends WordSpecLike
@@ -54,10 +71,12 @@ class AkkaHttpWebsocketTest extends WordSpecLike
   val twsActor = system.actorOf(Props[TestWebsocket])
   val twsActorLong = system.actorOf(Props[TestWebsocketLong])
   val twsActorStream = system.actorOf(Props[TestWebsocketStream])
+  val twsActorClose = system.actorOf(Props[TestWebsocketClose])
   // Wait for the actor to be up or routes will be empty
   Await.result(system.actorSelection(twsActor.path).resolveOne(), Duration("5 seconds"))
   Await.result(system.actorSelection(twsActorLong.path).resolveOne(), Duration("5 seconds"))
   Await.result(system.actorSelection(twsActorStream.path).resolveOne(), Duration("5 seconds"))
+  Await.result(system.actorSelection(twsActorClose.path).resolveOne(), Duration("5 seconds"))
   val routes = ExternalAkkaHttpRouteContainer.getRoutes.reduceLeft(_ ~ _)
   // End of setup
 
@@ -77,7 +96,7 @@ class AkkaHttpWebsocketTest extends WordSpecLike
           wsClient.expectMessage("Hello Peter! var1: friend")
 
           wsClient.sendMessage(BinaryMessage(ByteString("abcdef")))
-          wsClient.expectMessage(ByteString("Hello abcdef! var1: friend"))
+          wsClient.expectMessage("Hello abcdef! var1: friend")
 
           wsClient.sendMessage("John")
           wsClient.expectMessage("Hello John! var1: friend")
@@ -133,6 +152,23 @@ class AkkaHttpWebsocketTest extends WordSpecLike
 
           wsClient.sendCompletion()
           wsClient.expectCompletion()
+        }
+    }
+
+    "hit its closing method" in {
+      val wsClient = WSProbe()
+      // WS creates a WebSocket request for testing
+      WS("/close", wsClient.flow) ~> routes ~>
+        check {
+          isWebSocketUpgrade mustEqual true
+
+          wsClient.sendMessage("test")
+          wsClient.expectMessage("Close Actor")
+
+          wsClient.sendCompletion()
+          wsClient.expectCompletion()
+
+          ClosedObject.closed mustEqual true
         }
     }
   }
