666d1ad6804da038cb269664fdae8f089e52b485
Steve Plate <steve.plate@oracle.com>
2018-08-10 14:05:21 -0700

    Access Log, add the client IP, and remove the default derivation of the userId

--- src/main/scala/com/webtrends/harness/component/akkahttp/logging/AccessLog.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/logging/AccessLog.scala
@@ -2,6 +2,7 @@ package com.webtrends.harness.component.akkahttp.logging
 
 import akka.http.scaladsl.model.{DateTime, HttpRequest, StatusCode}
 import com.webtrends.harness.command.{BaseCommand, CommandBean}
+import com.webtrends.harness.component.akkahttp.AkkaHttpBase
 import com.webtrends.harness.component.akkahttp.AkkaHttpBase.TimeOfRequest
 import com.webtrends.harness.logging.Logger
 
@@ -9,23 +10,33 @@ trait AccessLog  {
   this: BaseCommand =>
 
   val accessLog = Logger("AccessLog")
-  def logAccess(request: HttpRequest, bean: CommandBean, statusCode: Option[StatusCode] = None) {
+
+  def getClientIP(bean: CommandBean): Option[String] ={
+    bean.getValue[Map[String,String]](AkkaHttpBase.RequestHeaders).flatMap (m =>
+    m.get("X_FORWARDED_FOR").orElse(m.get("REMOTE_ADDR")))
+  }
+
+  // Override to obtain the userId
+  def getUserId(bean: CommandBean): Option[String] = {
+    None
+  }
+
+  def logAccess(request: HttpRequest, bean: CommandBean, statusCode: Option[StatusCode]) {
 
     // modify the logback.xml file to write the "AccessLog" entries to a file without all of the prefix information
     //TODO add a config with an option to turn off logging
     try {
-      val host = request.headers.find(h=>h.is("host")).map(h=> h.value).getOrElse("-")
-      // the userId may be either a String or an Int
-      val user: String = bean.get("userId").map(id => id.toString).getOrElse("-")
-      val status:String = statusCode.map(sc => sc.intValue.toString).getOrElse("-")
+      val userId: String = getUserId(bean).getOrElse("-")
+      val client: String = getClientIP(bean).getOrElse("-")
+      val status: String = statusCode.map(sc => sc.intValue.toString).getOrElse("-")
       val responseTimestamp: Long = System.currentTimeMillis()
       val requestTimestamp: Long = bean.getValue[Long](TimeOfRequest).getOrElse(responseTimestamp)
       val elapsedTime: Long = responseTimestamp - requestTimestamp
       val now: String = DateTime(requestTimestamp).toIsoDateTimeString()
       /*
-          LogFormat "%v %l %u %t \"%r\" %>s %b %{ms}T"
+          LogFormat "%h %l %u %t \"%r\" %>s %b %{ms}T"
 
-          %v – The IP address of the server.
+          %h – The IP address of the client.
           %l – The identity of the client determined by identd on the client’s machine. Will return a hyphen (-) if this information is not available.
           %u – The userid of the client if the request was authenticated.
           %t – The time that the request was received, in UTC
@@ -36,7 +47,7 @@ trait AccessLog  {
 
           see https://httpd.apache.org/docs/2.4/logs.html
       */
-      accessLog.info( s"""$host - $user [$now] "${request.method.value} ${request.uri} ${request.protocol.value}" $status - $elapsedTime""")
+      accessLog.info( s"""$client - $userId [$now] "${request.method.value} ${request.uri} ${request.protocol.value}" $status - $elapsedTime""")
     } catch {
       case e: Exception =>
         log.error("Could not construct access log", e)
