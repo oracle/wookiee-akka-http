0c3c7fb13740fb17366981ecad7bffdd0ec56da2
Spencer Wood <spencer.wood@oracle.com>
2019-03-28 23:53:08 -0500

    Changes to enable split builds to actually work

--- bin/changeScalaVersion.sh
+++ bin/changeScalaVersion.sh
@@ -2,5 +2,5 @@
 
 echo "Changing Scala Version to $1, and Scala Artifact Version to $2"
 
-sed -i "s/<scala\.version>.*</<scala\.version>$1</" pom.xml
-sed -i "s/<scala\.artifact\.version>.*</<scala\.artifact\.version>$2</" pom.xml
+sed -i "s/<scala\.version>.*</<scala\.version>$1</" child-pom.xml
+sed -i "s/<scala\.artifact\.version>.*</<scala\.artifact\.version>$2</" child-pom.xml
