1f653220fa2995f08ad1754bf29aa6d39ac0038f
Spencer Wood <spencer.wood@oracle.com>
2019-05-02 16:21:37 -0500

    Change to single-pom model, much easier to work with

--- bin/changeScalaVersion.sh
+++ bin/changeScalaVersion.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Changing Scala Version to $1, and Scala Artifact Version to $2"
+projectName="wookiee-akka-http"
+echo "Changing $projectName Scala Version to $1, and Scala Artifact Version to $2"
 
-sed -i "s/<scala\.version>.*</<scala\.version>$1</" child-pom.xml
-sed -i "s/<scala\.artifact\.version>.*</<scala\.artifact\.version>$2</" child-pom.xml
+sed -i "s/<scala\.version>.*</<scala\.version>$1</" pom.xml
+sed -i "s/<scala\.artifact\.version>.*</<scala\.artifact\.version>$2</" pom.xml
+sed -i "s/<artifactId>$projectName.*</<artifactId>${projectName}_$2</" pom.xml
