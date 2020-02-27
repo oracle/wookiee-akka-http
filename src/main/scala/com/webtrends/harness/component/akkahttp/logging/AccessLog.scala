91463e3abb5742fe6fb41258a714d93d3c84b7fd
AdityaSharmaMalladi <vidyaditya.malladi@oracle.com>
2020-02-27 22:32:25 +0530

    Log origin

--- src/main/scala/com/webtrends/harness/component/akkahttp/logging/AccessLog.scala
+++ src/main/scala/com/webtrends/harness/component/akkahttp/logging/AccessLog.scala
@@ -42,7 +42,14 @@ trait AccessLog  {
 
           see https://httpd.apache.org/docs/2.4/logs.html
       */
-      accessLog.info( s"""${AccessLog.host} - $id [$requestTime] "${request.method.value} ${request.uri} ${request.protocol.value}" $status - $elapsedTime""")
+
+      val headers = request.headers
+      val origin = headers.find(header => header.name() == "Origin").map(_.value()).getOrElse("_")
+      val user_agent = headers.find(header => header.name() == "User-Agent").map(_.value()).getOrElse("_")
+
+      accessLog.info( s"""${AccessLog.host} - $id [$requestTime] "${request.method.value} ${request.uri} ${request.protocol.value}" $status - $elapsedTime - $origin - $user_agent""")
+
+//      accessLog.info( s"""${AccessLog.host} - $id [$requestTime] "${request.method.value} ${request.uri} ${request.protocol.value}" $status - $elapsedTime""")
     } catch {
       case e: Exception =>
         accessLog.error("Could not construct access log", e)
